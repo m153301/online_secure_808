d3bc945bdef975dd8fda8a547fa53509df057124
Mikoto Enomoto <mikoto@Mikotos-Mac.local>
2015-06-20 00:26:36 +0900

    AccessProcessorに変更

--- WEB-INF/src/dao/ItemDAO.java
+++ WEB-INF/src/dao/ItemDAO.java
@@ -5,44 +5,13 @@ package dao;
 /*****************************************************************************/
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import utility.DriverAccessor;
 import beans.Item;
 
-public class ItemDAO{
-
-	private final static String DRIVER_URL =
-		"jdbc:mysql://localhost:3306/secure?useUnicode=true&characterEncoding=UTF-8";
-
-	private final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-
-	private final static String USER_NAME = "mikoto";
-
-	private final static String PASSWORD = "110hadoru";
-
-	public Connection createConnection(){
-		try{
-			Class.forName(DRIVER_NAME);
-			Connection con = DriverManager.getConnection(DRIVER_URL, USER_NAME, PASSWORD);
-			return con;
-		} catch(ClassNotFoundException e){
-			System.out.println("Can't Find JDBC Driver.\n");
-
-		} catch(SQLException e){
-			System.out.println("Connect Error.\n");
-		}
-		return null;
-	}
-
-	public void closeConnection(Connection con){
-
-		try{
-			con.close();
-		}catch(Exception ex){}
-	}
-
+public class ItemDAO extends DriverAccessor{
 
 	public void registItemInfoDAO(Item item, Connection connection){
 
