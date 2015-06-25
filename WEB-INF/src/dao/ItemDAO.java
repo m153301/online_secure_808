373e951f08a73e944e44f254b691ef0ceac3ffcb
Mikoto Enomoto <mikoto@Mikotos-Mac.local>
2015-06-25 11:12:52 +0900

    itemIDをとってこれるようにした

--- WEB-INF/src/dao/ItemDAO.java
+++ WEB-INF/src/dao/ItemDAO.java
@@ -15,9 +15,8 @@ import beans.Item;
 
 public class ItemDAO extends DriverAccessor{
 
-	public int registItemInfoDAO(Item item, Connection connection){
-
-		int item_id = 0;
+	public void registItemInfoDAO(Item item, Connection connection){
+		//int item_id = 0;
 		try{
 
 			String sql = "insert into item values(?,?,?,?)";
@@ -29,16 +28,16 @@ public class ItemDAO extends DriverAccessor{
 			stmt.setInt(3, item.getItemPrice());
 			stmt.setInt(4, item.getItemStock());
 
-			String sql2 = "select item_id from item where item_name = '?'";
-			Statement stmt2 = connection.createStatement();
-			ResultSet rs = stmt2.executeQuery(sql2);
-			stmt.setString(1, item.getItemName());
+			//String sql2 = "select item_id from item where item_name = '?'";
+			//Statement stmt2 = connection.createStatement();
+			//ResultSet rs = stmt2.executeQuery(sql2);
+			//stmt.setString(1, item.getItemName());
 
-			rs.next();
-			item_id = rs.getInt("item_id");
+			//rs.next();
+			//item_id = rs.getInt("item_id");
+			stmt.executeUpdate();
 
-
-
+			stmt.close();
 		}catch(SQLException e){
 
 			e.printStackTrace();
@@ -46,6 +45,30 @@ public class ItemDAO extends DriverAccessor{
 		} finally {
 
 		}
-	return item_id;
+		//return item_id;
 	}
+
+	public int selectItemIdDAO(String item_name, Connection connection){
+
+		int item_id=0;
+		try{
+
+			String sql = "select item_id from item where item_name = '"+ item_name +"'";
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+
+			rs.next();
+			item_id = rs.getInt("item_id");
+
+			stmt.close();
+		}catch(SQLException e){
+
+			e.printStackTrace();
+
+		} finally {
+
+		}
+		return item_id;
+	}
+
 }
